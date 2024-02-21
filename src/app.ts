19795812d8caf5579c9daf3f668edfd53f0ef4e3
Yamisan0 <idris.messaad@gmail.com>
2024-02-21 16:37:49 +0100

    doc: cleaning and documenting code

--- src/app.ts
+++ src/app.ts
@@ -2,13 +2,14 @@ import http, { IncomingMessage, ServerResponse } from "http";
 import { DataInteractionService } from "./api/services/databaseServices";
 import cron from "node-cron";
 import dotenv from "dotenv";
-
-// Assuming these are implemented elsewhere in your project
 import { justifyTextController } from "./api/controllers/textController";
 import { generateToken } from "./api/controllers/authController";
 
 dotenv.config();
 
+/**
+ * Handles incoming HTTP requests and routes them to the appropriate controller functions.
+ */
 const requestListener = (req: IncomingMessage, res: ServerResponse) => {
   if (req.url === "/api/token" && req.method === "POST") {
     generateToken(req, res);
@@ -20,6 +21,9 @@ const requestListener = (req: IncomingMessage, res: ServerResponse) => {
   }
 };
 
+/**
+ * Schedule a task to reset the daily counter at midnight.
+ */
 cron.schedule(
   "0 0 * * *",
   async () => {
